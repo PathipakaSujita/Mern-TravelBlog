f1dddbb48a55455822a5e720a8be57963eee55d3
Pathipaka-Sujita <spathipaka@uh.edu>
2020-02-16 14:35:34 -0500

    add register user API

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user')
 
 mongoose.connect('mongodb+srv://mongodb_myTravelGuide:Mongodb@123@react-mern-travel-blog-6kmg7.mongodb.net/test?retryWrites=true&w=majority',
  {useNewUrlParser: true}).then(() => console.log("DB is connected"))
@@ -11,4 +14,18 @@ app.get('/', (req, res)  => {
 res.send("Hello World. This is my First mern boiler plate");
 });
 
+app.use(bodyParser.urlencoded({extended : true}) );
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req,res) => {
+   const user = new User(req.body);
+
+   user.save((err, userData) => {
+       if(err) return res.json({success:false, err})
+       return res.json({success: true, status:200})
+   })
+})
+
 app.listen(port);
+
