ba6e56e4bf5005df483cdda5cd1b3b611c36c230
Pathipaka-Sujita <spathipaka@uh.edu>
2020-02-26 20:22:54 -0500

    login logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -6,27 +7,74 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { User } = require('./models/user')
 const config =require('./config/keys.js');
+const { auth } = require('./middleware/auth.js');
 
 mongoose.connect(config.mongoURI,
  { useNewUrlParser: true,  useUnifiedTopology: true}).then(() => console.log("DB IS connected"))
                          .catch(err => console.error(err));
 
+                         app.use(bodyParser.urlencoded({extended : true}) );
+                         app.use(bodyParser.json());
+                         app.use(cookieParser());
+                         
+
 app.get('/', (req, res)  => {
 res.send("Hello World. This is my First mern boiler plate");
 });
 
-app.use(bodyParser.urlencoded({extended : true}) );
-app.use(bodyParser.json());
-app.use(cookieParser());
+
+app.get('/api/user/auth', auth , (req, res) => {
+    res.status(200).json({
+        _id : req._id,
+        isAuth : true,
+        email: req.user.email,
+        firstname: req.user.firstname,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
 
 app.post('/api/users/register', (req,res) => {
    const user = new User(req.body);
 
    user.save((err, userData) => {
-       if(err) return res.json({success:false, err})
-       return res.json({success: true, status:200})
-   })
-})
+       if(err) return res.json({success:false, err});
+        res.status(200).json({success: true, userData:userData})
+   });
+});
+
+
+app.post('/api/user/login', (req,res) => {
+    // find email in db
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user) 
+        return res.json({
+            loginSuccess : false, 
+            message: 'Auth Failed- Email not found'
+        });
+            //compare Password
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+            return res.json({
+                loginSuccess: false,
+                message:'Auth Failed - wrong password'
+            })
+
+            //Generate Token
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res
+                .cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                        loginSuccess: true,
+                        userId: user._id
+                });
+        });
+        });
+    });
+});
 
 app.listen(port);
 
